57e9b1a0f13f8eb05a2938651efc215e96596aad
anishsridharan <a.sridharan@starnustech.com>
2023-06-19 11:45:46 +0200

    Working with ros2 humble

--- launch/roboteq_diff_driver.launch.py
+++ launch/roboteq_diff_driver.launch.py
@@ -28,22 +28,22 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # ROS packages
-    pkg_roboteq = get_package_share_directory('roboteq_ros2_diff_driver')
+    pkg_roboteq = get_package_share_directory('roboteq_diff_driver')
 
     # Config
-    roboteq_config = os.path.join(pkg_roboteq, 'config/roboteq',
+    roboteq_config = os.path.join(pkg_roboteq, 'config',
         'roboteq.yaml')
 
     # Nodes
-    roboteq_ros2_diff_driver = Node(
-        package='roboteq_ros2_diff_driver',
-        executable='driver_dev',
-        name='roboteq_ros2_diff_driver',
+    roboteq_diff_driver = Node(
+        package='roboteq_diff_driver',
+        executable='roboteq_diff_driver',
+        name='roboteq_diff_driver',
         output='screen',
         parameters=[roboteq_config],
     )
 
     return LaunchDescription([
         # Nodes
-        roboteq_ros2_diff_driver,
+        roboteq_diff_driver,
     ])
